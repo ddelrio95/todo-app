8bd21a93b325281cc377e8a9f3c5ef833001ad91
Dario del Rio <ddelrio95@yahoo.com>
2021-11-12 22:30:37 -0500

    2

---
+++ firebase.js
@@ -0,0 +1,15 @@
+var firebaseConfig = {
+    apiKey: 'AIzaSyB-yfddTXrFwxYBZlRXBS2B8Hl4OErELVY',
+    authDomain: 'todo-list-768a2.firebaseapp.com',
+    projectId: 'todo-list-768a2',
+    storageBucket: 'todo-list-768a2.appspot.com',
+    messagingSenderId: '209142654072',
+    appId: '1:209142654072:web:a9a2e570eb3f38d523f98d',
+    measurementId: 'G-PM64GLXV39',
+}
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+firebase.Analytics(app)
+
+var db = firebase.firestore()
--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function addItem(event) {
     event.preventDefault()
-    console.log(event)
     let text = document.getElementById('todo-input')
-    console.log(text.value)
+    db.collection('todo-items').add({
+        text: text.value,
+        status: 'active',
+    })
+    text.value = ''
 }
